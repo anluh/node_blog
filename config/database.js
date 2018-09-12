69de87641230c775386511a49c8af4a551ad5b28
Andrii Luhvishchyk <andrii.luhvishchyk@smile-ukraine.com>
2018-09-12 16:33:14 +0300

    db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
+// module.exports = {
+//   database: 'mongodb://localhost:27017/node_learn',
+//   secret: 'yoursecret'
+// };
 module.exports = {
-  database: 'mongodb://localhost:27017/node_learn',
+  database: 'mongodb://admin:qwe123@ds255332.mlab.com:55332/heroku_z9bqn6g0',
   secret: 'yoursecret'
 };
